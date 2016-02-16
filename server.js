fddbf8f46751c6de39e53dbb3bca0c5dc679897d
Rakesh Nayak <rakesh.nayak@in.tesco.com>
2016-02-17 04:43:56 +0530

    added process env

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import app from './app';
 
 let debug = require('debug');
 debug = debug('test-api');
-
-app.set('port', 3001);
+var port = Number(process.env.PORT || 3001);
+app.set('port', port);
 
 const server = app.listen(app.get('port'), () => {
   console.log('MOCK API'); //eslint-disable-line no-console
